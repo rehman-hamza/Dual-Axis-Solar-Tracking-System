03f338ae456b0fbf44d96a35904440af354a1335
rehman-hamza <105734244+rehman-hamza@users.noreply.github.com>
2024-01-20 00:18:49 +0500

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -3,14 +3,16 @@
 #include "uRTCLib.h"
 #include <LiquidCrystal.h>
 
-uRTCLib rtc(0x68);
+// uRTCLib rtc; // Uncomment this line if you are using the default constructor
+uRTCLib rtc(0x68); // Use this line to create an instance with a specific address
+
 int hrr = 0;
 int minn = 0;
 int secc = 0;
 int a = 0;
 String weekname = "";
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-                             "Friday", "Saturday"};
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // RS, EN, D4, D5, D6, D7
 Servo myservo, myservo1;
 
@@ -19,10 +21,16 @@ void setup() {
   lcd.begin(16, 2);
   myservo.attach(9);
   myservo1.attach(10);
-  delay(1000); // wait for console opening
+  delay(1000); // Wait for console opening
   URTCLIB_WIRE.begin();
   myservo.write(0);
-  rtc.set(00, 56, 10, 5, 24, 7, 23); // set date & time
+
+  // Comment out below line once you set the date & time.
+  // Following line sets the RTC with an explicit date & time
+  // for example to set January 13, 2022, at 12:56 you would call:
+  rtc.set(00, 56, 10, 5, 24, 7, 23);
+  // rtc.set(second, minute, hour, dayOfWeek, dayOfMonth, month, year)
+  // set day of week (1=Sunday, 7=Saturday)
 }
 
 void loop() {
@@ -42,6 +50,7 @@ void loop() {
   if (secc < 10)
     lcd.print('0');
   lcd.print(secc);
+
   delay(1000);
   chkalarm();
 }
@@ -53,6 +62,7 @@ void chkalarm() {
   } else if ((rtc.hour() == 06 && rtc.minute() == 00 && rtc.second() < 4)) {
     myservo.write(97);
     myservo1.write(70);
+  }
   } else if ((rtc.hour() == 07 && rtc.minute() == 00 && rtc.second() < 4)) {
     myservo.write(109);
   } else if ((rtc.hour() == 11 && rtc.minute() == 00 && rtc.second() < 4)) {
@@ -109,7 +119,7 @@ void chkalarm() {
     myservo1.write(59);
   } else if ((rtc.hour() == 21 && rtc.minute() == 00 && rtc.second() < 4)) {
     // additional code
-  } else {
+else {
     lcd.print("");
   }
 }
